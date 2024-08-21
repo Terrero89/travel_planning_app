1a8e2abe637902e3db076684e1098166ee5e4200
Terrero89 <sergio-terrero@hotmail.com>
2024-08-20 21:26:12 -0400

    header component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 "use client"
 
 import Trips, { Places } from '../../types/trips';
+import UIHeader from './components/UI/UIHeader';
 import { useTripsStore } from './stores/useStore';
 
 
@@ -12,10 +13,10 @@ export default function Home() {
   return (
     <main className="">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
-        <div>HEADER</div>
-        <p>TRIPS: {trip.map((trip: Trips) => trip.destination)}</p>
-        <p>CITIES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.city))}</p>
-        <p>EXPENSES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.expenses.map((expense: any) => expense.expense)))}</p>
+        <UIHeader title="TRAVEL PLANNING APP"/>
+        <div>TRIPS: {trip.map((trip: Trips) => trip.destination)}</div>
+        <div>CITIES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.city))}</div>
+        <div>EXPENSES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.expenses.map((expense: any) => expense.expense)))}</div>
       </div>
     </main>
   );
