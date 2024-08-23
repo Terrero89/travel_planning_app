ab83e0877f733fcfe59b4578dca094d73b3f2c7d
Sergio Terrero <60996043+Terrero89@users.noreply.github.com>
2024-08-22 22:13:52 -0400

    Revert "Destination components"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 
 "use client"
 
-import { Places, Trips } from '../../types/trips';
-import CityItem from './components/cities/CityItem';
-import CityList from './components/cities/CityList';
-import DestinationItem from './components/destination/DestinationItem';
-import DestinationList from './components/destination/DestinationList';
-import ExpensesList from './components/expenses/ExpensesList';
+import Trips, { Places } from '../../types/trips';
 import UIHeader from './components/UI/UIHeader';
 import { useTripsStore } from './stores/useStore';
 
@@ -19,13 +14,9 @@ export default function Home() {
     <main className="">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
         <UIHeader title="TRAVEL PLANNING APP"/>
-        DESTINATIONS:
-        <DestinationList  />
-        CITY LIST: 
-        <CityList/>
-        EXPENSES
-        <ExpensesList/>
-        
+        <div>TRIPS: {trip.map((trip: Trips) => trip.destination)}</div>
+        <div>CITIES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.city))}</div>
+        <div>EXPENSES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.expenses.map((expense: any) => expense.expense)))}</div>
       </div>
     </main>
   );
--- types/trips.tsx
+++ types/trips.tsx
@@ -1,44 +1,37 @@
-export type TransportType = 'Flight' | 'Car' | 'Bus';
+export type TransportType = 'Flight' | 'Drive' | 'Bus';
 
-export type Trips =  {
-    destinationID: string;
-    destination: string;
-    transportType: TransportType;
-    from: Date | string;
-    to: Date | string;
-    destinationBudget:number; // based on the total cost of the trip
-    tripDuration: 12,
-    dateAdded: Date | string;
-    isTripCompleted: boolean;
-    tripRating: number;
-    places: Places[];
-}
+
 export type Expenses = {
-    expense: string;
     expenseID: string;
     category: string;
     expenseName: string;
     cost: number;
     date: Date | string;
-    startTime: Date | string;
-    endTime: Date | string;
     duration: number;
     comments: string;
     isCompleted: Boolean
-    placerRating: number;
 }
 
 export type Places = {
-    cost: ReactNode;
-    category: string;
     cityID: string;
     city: string;
     from: Date | string;
     to: Date | string;
     totalCost: number;
     isThisCityVisited: boolean;
-    cityRating: number;
     expenses: Expenses[];
 }
 
-
+export type Trips =  {
+    tripID: string;
+    tripName: string;
+    transportType: TransportType;
+    from: Date | string;
+    to: Date | string;
+    totalCost: number;
+    arriving: Date | string;
+    leaving: Date | string;
+    tripDuration: 12,
+    dateAdded: Date | string;
+    places: Places[];
+}
