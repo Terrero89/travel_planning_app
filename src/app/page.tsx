c3e9a6802aef858ff10a8683247c4fdcae4797f9
Sergio Terrero <60996043+Terrero89@users.noreply.github.com>
2024-08-24 10:32:07 -0400

    Revert "UI buttons"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,24 @@
 
 "use client"
 
-import { Places, Trips } from '../../types/trips';
-import { calculateDaysRemaining, calculateTimeDifference} from '../../utils/date-convertion';
-import CityItem from './components/cities/CityItem';
-import CityList from './components/cities/CityList';
-import DestinationItem from './components/destination/DestinationItem';
-import DestinationList from './components/destination/DestinationList';
-import ExpensesList from './components/expenses/ExpensesList';
+import Trips, { Places } from '../../types/trips';
 import UIHeader from './components/UI/UIHeader';
 import { useTripsStore } from './stores/useStore';
 
 
 
 export default function Home() {
-
-  const trip = useTripsStore((state: { trip: Trips[]; }) => state.trip);
-
-
-
   
+  const trip = useTripsStore((state: { trip: Trips[]; }) => state.trip);
   return (
-
-
-
-  <main className="">
-    <div className="mx-auto z-10 w-full max-w-3xl items-center justify-between font-mono text-sm ">
-      <UIHeader title="Travel Planning App" />
-      {calculateDaysRemaining("2025-02-28")} days remaining----- {calculateTimeDifference("10:55 AM", "10:55 PM")}
-      <DestinationList />
-      CITY LIST:
-      <CityList />
-      EXPENSES
-      <ExpensesList />
-
-    </div>
-  </main>
+    <main className="">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
+        <UIHeader title="TRAVEL PLANNING APP"/>
+        <div>TRIPS: {trip.map((trip: Trips) => trip.destination)}</div>
+        <div>CITIES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.city))}</div>
+        <div>EXPENSES: {trip.map((trip: Trips) => trip.places.map((place: Places) => place.expenses.map((expense: any) => expense.expense)))}</div>
+      </div>
+    </main>
   );
 }
 
